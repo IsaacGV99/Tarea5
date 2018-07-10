a9bd0b6ba267183878d2fd6499017aafa8841e70
Jose Isaac Gallegos Velica <39529793+IsaacGV99@users.noreply.github.com>
2018-07-09 19:18:34 -0500

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -6,12 +6,12 @@ typedef int(*Comparator)(Type, Type);
 typedef unsigned long(*Index)(Type);
 //typedef Type(*Clone)(Type);
 //typedef void (*MyFree)(Type);
-//typedef void (*MyPrint)(Type);
+typedef void (*MyPrint)(Type);
 
 typedef struct strGraph *Graph;
 typedef enum{false, true}boolean;
 
-Graph graph_create(Comparator Cmp, Index Indice/*, Clone clone, MyFree myfree, MyPrint myprint*/);
+Graph graph_create(Comparator Cmp, Index Indice/*, Clone clone, MyFree myfree*/, MyPrint myprint);
 void graph_destroy(Graph who);
 boolean graph_addVertex(Graph who, Type data);
 boolean graph_addEdge(Graph who, unsigned long source, unsigned long sink);
