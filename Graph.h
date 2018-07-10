eb972d0405495ff8dcf5851e900503e5d01ae7de
Jose Isaac Gallegos Velica <39529793+IsaacGV99@users.noreply.github.com>
2018-07-09 20:45:36 -0500

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -14,11 +14,11 @@ typedef enum{false, true}boolean;
 Graph graph_create(Comparator Cmp, Index Indice/*, Clone clone, MyFree myfree*/, MyPrint myprint);
 void graph_destroy(Graph who);
 boolean graph_addVertex(Graph who, Type data);
-boolean graph_addEdge(Graph who, unsigned long source, unsigned long sink);
+boolean graph_addEdge(Graph who, Type source, Type sink);
 unsigned long graph_vertexCount(Graph who);
 unsigned long graph_edgeCount(Graph who);
-unsigned long graph_outDegree(Graph who, unsigned long source);
-boolean graph_hasEdge(Graph who, unsigned long source, unsigned long sink);
-void graph_print(Graph who, MyPrint printFunction);
+unsigned long graph_outDegree(Graph who, Type source);
+boolean graph_hasEdge(Graph who, Type source, Type sink);
+void graph_print(Graph who);
 
 #endif /* GRAPH_H_ */
