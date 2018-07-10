f97a5bf68ea5721a90693daac1c154d4adb0aaf7
agui197 <33585691+agui197@users.noreply.github.com>
2018-07-10 17:22:37 -0500

    Update useGraph.c

--- useGraph.c
+++ useGraph.c
@@ -6,11 +6,20 @@
 int compareString(Type, Type);
 void printString(Type data);
 
+struct strPerson{
+	char[20] actor;
+	int year;
+	char[50] movie;
+};
+
 int compareString(Type data1, Type data2){
-	char v1=*(char *)data1;
-	char v2=*(char *)data2;
-	return strcmp(v1,v2;
+	char* v1=*(struct strPerson *)data1;
+	char* v2=*(struct strPerson *)data2;
+	return strcmp(v1.actor,v2.actor);
 }
 void printString(Type data){
-	printf(" %s ", *(char *)data);
+	struct strPerson v1=(struct strPerson *)data;
+	printf(" %s ", v1.actor);
+	printf(" %d ", v1.year);
+	printf(" %s ", v1.movie);
 }
