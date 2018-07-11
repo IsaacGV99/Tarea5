49f20d572133b3bd78b8c159493dabe624250cb8
agui197 <33585691+agui197@users.noreply.github.com>
2018-07-10 19:49:44 -0500

    Update useGraph.c

--- useGraph.c
+++ useGraph.c
@@ -7,16 +7,12 @@ int compareString(Type, Type);
 void printString(Type data);
 
 
-struct strPerson{
-	char[20] actor;
+struct strVertex{
+	char texto1[20];
 	int year;
-	char[50] movie;
+	char texto2[50];
 };
-struct strMovie{
-	char[20] movie;
-	int year;
-	char[50] sinapsis;
-};
+
 
 int main(){
 	FILE *vertex_personFile;
@@ -24,29 +20,67 @@ int main(){
 	FILE *edgesPersonFile;
 	vertex_personFile=fopen("vertex_person.csv","r");
 	vertex_moviesFile=fopen("vertex_movies.csv","r");
-	edgesPersonFile=fopen("edgesPerson.csv","r");
+	edgesPersonFile=fopen("edges_PersonMovies.csv","r");
 	if(vertex_personFile==NULL||vertex_moviesFile==NULL||edgesPersonFile==NULL)
 		printf("Error al abrir el archivo\n");
 	else{
-		char buff[100];
+		char buff[20]="";
+		char buff2[10];
+		char buff3[100];
 		int i=0;
-		while(buff!=","){
-			fscanf(vertex_personFile, "%c",&buff[i]);
+		char c;
+		//!feof(vertex_personFile)
+		while((c=fgetc(vertex_personFile))!=','&&i<20&&c!=EOF){
+			buff[i]=c;
 			i++;
-		}				 
+		}
+		while((c=fgetc(vertex_personFile))!=','&&i<20&&c!=EOF){
+			buff[i]=c;
+			i++;
+		}
+
+		printf("%s",buff);
+		/*
+		fscanf(vertex_personFile, "%c",&buff);
+		printf("%c",buff);
+		printf("success");
+		*/
+		/*
+		int i=0;
+		while(!feof(vertex_personFile)){
+			while(buff[i]!=','){
+				fscanf(vertex_personFile, "%c",&buff[i]);
+				printf("%c",buff[i]);
+				i++;
+			}
+			while(buff[i]!=','){
+				fscanf(vertex_personFile, "%c",&buff2[i]);
+				i++;
+			}
+			while(buff[i]!='\n'){
+				fscanf(vertex_personFile, "%c",&buff3[i]);
+				i++;
+			}
+			printf("%s",buff);
+			printf("%s",buff2);
+			printf("%s",buff3);
+		}
+
+	*/
 	}
+
 	return 0;
 }
 
 
 int compareString(Type data1, Type data2){
-	struct strPerson v1=*(struct strPerson *)data1;
-	struct strPerson v2=*(struct strPerson *)data2;
-	return strcmp(v1.actor,v2.actor);
+	struct strVertex v1=*(struct strVertex *)data1;
+	struct strVertex v2=*(struct strVertex *)data2;
+	return strcmp(v1.texto1,v2.texto1);
 }
 void printString(Type data){
-	struct strPerson v1=*(struct strPerson *)data;
-	printf(" %s ", v1.actor);
+	struct strVertex v1=*(struct strVertex *)data;
+	printf(" %s ", v1.texto1);
 	printf(" %d ", v1.year);
-	printf(" %s ", v1.movie);
+	printf(" %s ", v1.texto2);
 }
